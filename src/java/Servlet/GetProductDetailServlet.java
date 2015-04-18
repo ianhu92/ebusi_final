993b79a00a641d0706142798b44f3aa0c6e32f90
ylj816 <ylijia1991@gmail.com>
2015-04-17 21:59:17 -0400

    ..
    update

--- src/java/Servlet/GetProductDetailServlet.java
+++ src/java/Servlet/GetProductDetailServlet.java
@@ -5,8 +5,14 @@
  */
 package Servlet;
 
+import JavaBean.DBbean;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,9 +33,33 @@ public class GetProductDetailServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String productid =request.getParameter("productid");
+        String sql="Select pid,pname,price,stock,img,des, from product where product.pid=inventory.pid  and pid='"+productid+"'";
+        String url="";
+        DBbean db=new DBbean();
+        try {
+            ResultSet rs=db.query(sql);
+            ArrayList productlist=new ArrayList();
+            while (rs.next())
+            {
+                JSONObject product=new JSONObject();
+                product.append("productid", rs.getString(1));
+                product.append("productname", rs.getString(2));
+                product.append("price", rs.getString(3));
+                product.append("inventory", rs.getString(4));
+                product.append("img", rs.getString(5));
+                product.append("description", rs.getString(6));
+                productlist.add(product);
+            }
+            request.setAttribute("product", productlist);
+            RequestDispatcher rd=request.getRequestDispatcher(url);
+            rd.forward(request, response);
+            
+        } catch (Exception ex) {
+            Logger.getLogger(GetProductServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
     }
 
     
 
-}
--- src/java/Servlet/GetProductServlet.java
+++ src/java/Servlet/GetProductServlet.java
@@ -5,14 +5,21 @@
  */
 package Servlet;
 
+import JavaBean.DBbean;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import org.json.JSONException;
+import org.json.JSONObject;
 /**
  *
  * @author yanglijia
@@ -20,69 +27,37 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "GetProductServlet", urlPatterns = {"/GetProductServlet"})
 public class GetProductServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet GetProductServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet GetProductServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        String type=request.getParameter("category");
+        String sql="Select pid,pname,price,img,stock from product where product.pid=inventory.pid  and type='"+type+"'";
+        String url="";
+        DBbean db=new DBbean();
+        try {
+            ResultSet rs=db.query(sql);
+            ArrayList productlist=new ArrayList();
+            while (rs.next())
+            {
+                JSONObject product=new JSONObject();
+                product.append("productid", rs.getString(1));
+                product.append("productname", rs.getString(2));
+                product.append("price", rs.getString(3));
+                product.append("inventory", rs.getString(5));
+                product.append("img", rs.getString(4));
+                productlist.add(product);
+            }
+            request.setAttribute("product", productlist);
+            RequestDispatcher rd=request.getRequestDispatcher(url);
+            rd.forward(request, response);
+            
+        } catch (Exception ex) {
+            Logger.getLogger(GetProductServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+    
 
 }
