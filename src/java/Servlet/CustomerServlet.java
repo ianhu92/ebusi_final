50b5dfa84a0af29d338e150ec4ac89cc7c52f354
violet1210 <violet.liu1210@gmail.com>
2015-04-18 20:29:54 -0400

    customer

---
+++ src/java/Servlet/CustomerServlet.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlet;
+
+import JavaBean.DBbean;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author yanglijia
+ */
+@WebServlet(name = "CustomerServlet", urlPatterns = {"/CustomerServlet"})
+public class CustomerServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+           String username=(String)request.getSession().getAttribute("username");
+           DBbean db=new DBbean();
+           
+           String sql="select firstname,lastname,address,phone number from customer where cname='"+username+"'";
+            try {
+                ResultSet rs=db.query(sql);
+                JSONObject productlist=new JSONObject();
+                if (rs.next()){
+                    JSONObject product=new JSONObject();
+                    product.append("firstname",rs.getString("firstname"));
+                    product.append("lastname",rs.getString("lastname"));
+                    product.append("address",rs.getString("address"));
+                    product.append("phonenumber",rs.getString("phonenumber"));
+                    productlist.put("product", product);
+                    
+                    out.print(productlist.toString());       
+                }
+                else{
+                    out.print("has no such userid");
+                }
+            } catch (Exception ex) {
+                Logger.getLogger(CustomerServlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+           
+           
+           
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
